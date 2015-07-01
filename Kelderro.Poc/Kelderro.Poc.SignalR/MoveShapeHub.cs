4de9c91e817d0f482851f7fa4c4601931d41f1da
Kelderro <info@kelderro.nl>
2015-07-01 12:21:32 +0000

    Add SignalR demo

---
+++ Kelderro.Poc/Kelderro.Poc.SignalR/MoveShapeHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading;
+
+namespace Kelderro.Poc.SignalR
+{
+    public class MoveShapeHub : Hub
+    {
+        // Is set via the constructor on each creation
+        private Broadcaster _broadcaster;
+        public MoveShapeHub()
+            : this(Broadcaster.Instance)
+        {
+        }
+        public MoveShapeHub(Broadcaster broadcaster)
+        {
+            _broadcaster = broadcaster;
+        }
+        public void UpdateModel(ShapeModel clientModel)
+        {
+            clientModel.LastUpdatedBy = Context.ConnectionId;
+            // Update the shape model within our broadcaster
+            _broadcaster.UpdateShape(clientModel);
+        }
+    }
+}
